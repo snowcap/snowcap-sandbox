3e3e8f96aae47c97b1d595035da31cca9012b9c4
pvanliefland <pierre@snowcap.be>
2012-11-01 15:52:20 +0100

    Updated to Symfony 2.1.3

--- web/app.php
+++ web/app.php
@@ -5,9 +5,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
+// Use APC for autoloading to improve performance
+// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
 /*
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
