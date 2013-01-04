33c1ad3e5410988ab91fd5c751e04151004bb2c4
pvanliefland <pierre@snowcap.be>
2013-01-04 18:10:13 +0100

    (WIP) Datalist demo bundle

--- src/Snowcap/DatalistDemoBundle/Controller/DefaultController.php
+++ src/Snowcap/DatalistDemoBundle/Controller/DefaultController.php
@@ -7,9 +7,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Snowcap\AdminBundle\Datalist\Datasource\DoctrineORMDatasource;
+use Snowcap\AdminBundle\Datalist\Datasource\ArrayDatasource;
 
 /**
- * @Route("/datagrid")
+ * @Route("/datalist")
  */
 class DefaultController extends Controller
 {
@@ -19,8 +20,15 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $datalistFactory = $this->getDatalistFactory();
-        $datalist = $datalistFactory->createBuilder()
+        return array();
+    }
+
+    /**
+     * @Template("SnowcapDatalistDemoBundle:Default:datalist.html.twig")
+     */
+    public function datalist1Action()
+    {
+        $datalist = $this->getDatalistFactory()->createBuilder('datalist', array('data_class' => 'Snowcap\DatalistDemoBundle\Entity\Player'))
             ->addField('firstName')
             ->addField('lastName')
             ->getDatalist();
@@ -28,12 +36,89 @@ class DefaultController extends Controller
         $queryBuilder = $this->getDoctrine()->getEntityManager()->createQueryBuilder()
             ->select('p')
             ->from('SnowcapDatalistDemoBundle:Player', 'p');
-
         $datasource = new DoctrineORMDatasource($queryBuilder);
+        $datasource
+            ->paginate()
+            ->setPage(1);
 
         $datalist->setDataSource($datasource);
 
-        return array('datalist' => $datalist);
+        return array(
+            'datalist_title' => 'Regular datalist with Doctrine ORM datasource',
+            'datalist' => $datalist,
+        );
+    }
+
+    /**
+     * @Template("SnowcapDatalistDemoBundle:Default:datalist.html.twig")
+     */
+    public function datalist2Action()
+    {
+        $datalist = $this->getDatalistFactory()->createBuilder()
+            ->addField('player')
+            ->addField('score')
+            ->getDatalist();
+
+        $datasource = new ArrayDatasource(array(
+            array(
+                'player' => 'PIE',
+                'score' => 5000,
+            ),
+            array(
+                'player' => 'JER',
+                'score' => 4900,
+            ),
+            array(
+                'player' => 'JER',
+                'score' => 4800,
+            ),
+            array(
+                'player' => 'PIE',
+                'score' => 4400,
+            ),
+            array(
+                'player' => 'EDW',
+                'score' => 4300,
+            ),
+            array(
+                'player' => 'ABD',
+                'score' => 4200,
+            ),
+            array(
+                'player' => 'JER',
+                'score' => 4100,
+            ),
+            array(
+                'player' => 'ABD',
+                'score' => 3845,
+            ),
+            array(
+                'player' => 'EDW',
+                'score' => 3680,
+            ),
+            array(
+                'player' => 'PIE',
+                'score' => 3360,
+            ),
+            array(
+                'player' => 'JER',
+                'score' => 3250,
+            ),
+            array(
+                'player' => 'EDW',
+                'score' => 3200,
+            ),
+            array(
+                'player' => 'JER',
+                'score' => 3000,
+            ),
+        ));
+        $datalist->setDatasource($datasource);
+
+        return array(
+            'datalist_title' => 'Regular datalist with array datasource',
+            'datalist' => $datalist
+        );
     }
 
     /**
