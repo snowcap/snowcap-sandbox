27d4dd58d96b0940932c0d5eb3e03f2fb1b765ea
pvanliefland <pierre@snowcap.be>
2013-05-09 12:29:24 +0200

    Updated snowcap bundles + added proper admin security

---
+++ src/Snowcap/AdminDemoBundle/Entity/AdminUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Snowcap\AdminDemoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Snowcap\AdminBundle\Entity\User;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table
+ */
+class AdminUser extends User
+{
+
+}
